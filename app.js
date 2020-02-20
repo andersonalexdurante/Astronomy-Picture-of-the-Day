7e20123e3eaa1d74bc47297a4e31427570b7596d
Anderson Alex Durante <anderson.alexdurante@gmail.com>
2020-02-20 00:13:00 -0300

    Showing Date and Image

--- app.js
+++ app.js
@@ -7,8 +7,23 @@ request.send();
 
 request.onload = function() {
     let image = request.response;
-    
-    console.log(image.response)
+   
+    main(image);
+}
+
+function main(json){
+  const data = document.querySelector('.date')
+  data.textContent = json.date
+  //const explanation = document.querySelector('.explanation')
+  //explanation.textContent = json.explanation
+  if(data.hdurl != null){
+    const image = document.querySelector('.image')
+    image.src = json.hdurl
+    const 
   }
-
-
+  else{
+    const image = document.querySelector('.image')
+    image.src = json.url
+    
+  }
+}
